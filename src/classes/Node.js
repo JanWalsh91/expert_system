96c800424c8483a2af5a29a8f6a02328c47bde3c
Jan WALSH <jwalsh@e3r7p12.42.fr>
2018-12-06 13:59:09 +0100

    rm comment

--- src/classes/Node.js
+++ src/classes/Node.js
@@ -27,7 +27,6 @@ class Node {
 		// if the fact exists and it is not undefined, return it. Else
 		if (Fact.keyExists(this.key)) {
 			let ret = Fact.evaluate(this.key)
-			// console.log(ret);
 			if (ret != undefined) {
 				return ret
 			}
